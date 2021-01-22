25c5394c3830e6b5ee31b513470f6a94f0957c06
Andradinho <raphaelandrade2013@gmail.com>
2021-01-22 13:35:06 -0300

    Criação de interface gráfica usando Swing.

---
+++ src/br/com/andradinho/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.andradinho.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.andradinho.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+	
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
