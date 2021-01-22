a93f85518f0aef0e8b991d70ce1dc76097874930
Andradinho <raphaelandrade2013@gmail.com>
2021-01-22 14:08:15 -0300

    Inserção de painel+ botões+Observers na interface gráfica.

---
+++ src/br/com/andradinho/cm/visao/BotaoCampo.java
@@ -0,0 +1,13 @@
+package br.com.andradinho.cm.visao;
+
+import javax.swing.JButton;
+
+import br.com.andradinho.cm.modelo.Campo;
+
+@SuppressWarnings("serial")
+public class BotaoCampo extends JButton {
+
+	public BotaoCampo(Campo campo) {
+		
+	}
+}
--- src/br/com/andradinho/cm/visao/PainelTabuleiro.java
+++ src/br/com/andradinho/cm/visao/PainelTabuleiro.java
@@ -1,5 +1,7 @@
 package br.com.andradinho.cm.visao;
 
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 import br.com.andradinho.cm.modelo.Tabuleiro;
@@ -9,5 +11,12 @@ public class PainelTabuleiro extends JPanel {
 	
 	public PainelTabuleiro(Tabuleiro tabuleiro) {
 		
+		setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+		
+		tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+		
+		tabuleiro.registrarObservador(e -> {
+			// TODO mostrar o resultado ao usuário
+		});
 	}
 }
